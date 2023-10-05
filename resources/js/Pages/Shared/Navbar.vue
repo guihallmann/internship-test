ba46101f59d6ca501df6078ab4d9107464630bab
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-05 14:24:06 -0300

    feat: create navbar component

---
+++ resources/js/Pages/Shared/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="bg-zinc-800 p-4">
+        <div class="container mx-auto flex justify-between items-center">
+            <div>
+                <span class="text-white text-3xl font-bold">I.T.S</span>
+            </div>
+            <div>
+                <nav-link
+                    class="text-xl text-white"
+                    href="/logout"
+                    method="post"
+                >
+                    Sair
+                </nav-link>
+            </div>
+        </div>
+    </nav>
+</template>
+<script>
+import NavLink from "./NavLink.vue";
+export default {
+    components: { NavLink },
+};
+</script>
