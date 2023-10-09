ee29bf807165971c97323539bff74effc9aaa616
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-09 12:47:04 -0300

    feat: create people page to render all people

---
+++ resources/js/Pages/People.vue
@@ -0,0 +1,28 @@
+<template>
+    <layout>
+        <div class="flex flex-col">
+            <person-card
+                v-for="person in people"
+                :key="person.id"
+                :person="person"
+            />
+        </div>
+
+        <!-- <create-person></create-person> -->
+    </layout>
+</template>
+<script>
+import Layout from "./Shared/Layout.vue";
+import CreatePerson from "./Shared/Forms/CreatePerson.vue";
+import PersonCard from "./Shared/PersonCard.vue";
+export default {
+    components: {
+        Layout,
+        CreatePerson,
+        PersonCard,
+    },
+    props: {
+        people: Array,
+    },
+};
+</script>
