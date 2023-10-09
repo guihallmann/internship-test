c4605933e5cb7633c53ea1fcbc7f203231a3c452
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-09 12:48:29 -0300

    feat: create a default layout with the navbar

---
+++ resources/js/Pages/Shared/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <section>
+        <header>
+            <navbar></navbar>
+        </header>
+    </section>
+    <section>
+        <slot />
+    </section>
+</template>
+<script>
+import Navbar from "./Navbar.vue";
+export default {
+    components: { Navbar },
+};
+</script>
