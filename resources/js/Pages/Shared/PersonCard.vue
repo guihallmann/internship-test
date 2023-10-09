fd71a1b30b8079404755b0002b106c5010d3c28f
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-09 12:49:27 -0300

    feat: create a card to show a persons info

---
+++ resources/js/Pages/Shared/PersonCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <div
+        class="grid grid-rows-6 space-y-0.5 items-center justify-items-start border-2 border-zinc-800 rounded m-1 p-4 bg-zinc-100 font-normal text-xl hover:bg-zinc-200/80 sm:grid-cols-6 sm:grid-rows-none"
+    >
+        <span><strong>CPF: </strong>{{ person.cpf }}</span>
+        <span><strong>Nome: </strong>{{ person.name }}</span>
+        <span><strong>Nascimento: </strong>{{ person.birthday }}</span>
+        <span><strong>Sexo: </strong>{{ person.sex }}</span>
+        <span
+            ><strong>Endereço: </strong>
+            {{
+                [
+                    person.street ? `${person.street}` : "",
+                    person.number ? `nº ${person.number}` : "",
+                    person.neighborhood ? `${person.neighborhood}` : "",
+                    person.city ? `${person.city}` : "",
+                ]
+                    .filter(Boolean)
+                    .join(" - ")
+            }}
+        </span>
+        <div class="flex justify-evenly items-center w-full gap-4">
+            <nav-link
+                class="font-medium text-amber-500 hover:underline"
+                href="#"
+                as="button"
+            >
+                Editar
+            </nav-link>
+            <nav-link
+                class="font-medium text-rose-500 hover:underline"
+                href="#"
+                as="button"
+            >
+                Excluir
+            </nav-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import NavLink from "./NavLink.vue";
+export default {
+    components: { NavLink },
+    props: {
+        person: Object,
+    },
+};
+</script>
