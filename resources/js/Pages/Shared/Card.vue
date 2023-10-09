bb7574c35123fa0cf6a55863afd65cd17c2269ea
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-09 12:48:15 -0300

    feat: create a generic card component

---
+++ resources/js/Pages/Shared/Card.vue
@@ -0,0 +1,13 @@
+<template>
+    <Link
+        class="border-2 border-zinc-800 rounded px-10 py-6 bg-zinc-100 font-bold text-2xl hover:bg-zinc-200 m-2 w-full text-center sm:max-w-lg"
+    >
+        <slot />
+    </Link>
+</template>
+<script>
+import { Link } from "@inertiajs/vue3";
+export default {
+    components: { Link },
+};
+</script>
