f397f2a0184cb636da234c409f47de23ff83851d
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-11 10:13:30 -0300

    feat: add href and icons to home cards

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,18 +1,26 @@
 <template>
     <layout>
         <div class="flex flex-col justify-center items-center p-4 m-2">
-            <card href="/people">Contribuintes</card>
-            <card href="">Protocolos</card>
+            <card href="/create-person">
+                Contribuinte
+                <icon name="add-user" />
+            </card>
+            <card href="#">
+                Protocolo
+                <icon name="add-prot" />
+            </card>
         </div>
     </layout>
 </template>
 <script>
 import Card from "./Shared/Card.vue";
 import Layout from "./Shared/Layout.vue";
+import Icon from "./Shared/Icon.vue";
 export default {
     components: {
         Card,
         Layout,
+        Icon,
     },
 };
 </script>
