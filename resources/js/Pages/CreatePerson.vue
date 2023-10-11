66c357c50777e60f2be9172a19bb25dd8b43a656
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-11 10:14:04 -0300

    feat: add create person page

---
+++ resources/js/Pages/CreatePerson.vue
@@ -0,0 +1,20 @@
+<template>
+    <layout>
+        <div class="flex justify-center">
+            <create-person />
+        </div>
+
+        <!-- <create-person></create-person> -->
+    </layout>
+</template>
+<script>
+import Layout from "./Shared/Layout.vue";
+import CreatePerson from "./Shared/Forms/CreatePerson.vue";
+
+export default {
+    components: {
+        Layout,
+        CreatePerson,
+    },
+};
+</script>
