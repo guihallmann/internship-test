5087b225759d7cd821dd1bfbfc1794bd1daa1bdc
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-11 10:14:57 -0300

    feat: add update and destroy methos to person controller

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Person;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
 class PersonController extends Controller
 {
     public function index() {
-        // $all = Person::all()->map(fn($person) => ['name' => $person->name, 'id'=> $person->id]);
         $all = Person::all();
         return Inertia::render('People', ['people' => $all]);
     }
@@ -28,12 +30,43 @@ class PersonController extends Controller
         ]);
 
         Person::create($personDataValidation);
-
-        return redirect()->intended('/');
     }
 
     public function show(string $id) {
-        $person = Person::findOrfail($id);
-        return Inertia::render('EditPerson'. ['person' => $person]);
+        try {
+            $person = Person::findOrfail($id);
+            return Inertia::render('EditPerson', ['person' => $person]);
+        } catch(ModelNotFoundException $e) {
+            // implementar excessões depois
+            return;
+        }
+    }
+
+    public function update(Request $request, string $id) {
+        $person = Person::findOrFail($id);
+        $personDataValidation = $request->validate([
+            'name' => 'required|string',
+            'birthday' => 'required|date',
+            'cpf' => 'required|unique:people,cpf,' . $person->id,
+            'sex' => 'required|in:Masculino,Feminino,Outro',
+            'city' => 'nullable|string',
+            'neighborhood' => 'nullable|string',
+            'street' => 'nullable|string',
+            'number' => 'nullable|string',
+            'complement' => 'nullable|string',
+        ]);
+        Person::where('id', $id)->update($personDataValidation);
+        return redirect()->intended('/people');
+    }
+
+    public function destroy(string $id) {
+        try {
+            Person::where('id', $id)->delete();
+            return redirect()->intended('/people');
+        } catch(ModelNotFoundException $e) {
+            // implementar excessões depois
+            return;
+        }
+
     }
 }
