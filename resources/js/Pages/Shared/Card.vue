99fad693b10f5fb6607a6263767b070a1fed5cdc
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-11 10:13:09 -0300

    style: changing the style of the card

--- resources/js/Pages/Shared/Card.vue
+++ resources/js/Pages/Shared/Card.vue
@@ -1,6 +1,6 @@
 <template>
     <Link
-        class="border-2 border-zinc-800 rounded px-10 py-6 bg-zinc-100 font-bold text-2xl hover:bg-zinc-200 m-2 w-full text-center sm:max-w-lg"
+        class="flex justify-center items-center gap-3 border-2 border-zinc-800 rounded px-10 py-6 bg-zinc-100 font-bold text-2xl hover:bg-zinc-200 m-2 w-full text-center sm:max-w-md"
     >
         <slot />
     </Link>
