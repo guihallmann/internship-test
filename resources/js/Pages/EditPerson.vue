8eb8098572757425725853023bacade0c491f17e
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-11 10:13:45 -0300

    feat: create edit person page

---
+++ resources/js/Pages/EditPerson.vue
@@ -0,0 +1,23 @@
+<template>
+    <layout>
+        <div class="flex justify-center">
+            <edit-person :person="person"></edit-person>
+        </div>
+
+        <!-- <create-person></create-person> -->
+    </layout>
+</template>
+<script>
+import Layout from "./Shared/Layout.vue";
+import EditPerson from "./Shared/Forms/EditPerson.vue";
+
+export default {
+    components: {
+        Layout,
+        EditPerson,
+    },
+    props: {
+        person: Object,
+    },
+};
+</script>
