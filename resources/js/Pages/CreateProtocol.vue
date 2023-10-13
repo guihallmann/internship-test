fec2db8664d336bdaef2fe06a57865ef09c72089
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-12 21:51:02 -0300

    feat: add createProtocol page

---
+++ resources/js/Pages/CreateProtocol.vue
@@ -0,0 +1,22 @@
+<template>
+    <layout>
+        <div class="flex justify-center">
+            <create-protocol :people="people" />
+        </div>
+
+        <!-- <create-person></create-person> -->
+    </layout>
+</template>
+<script>
+import Layout from "./Shared/Layout.vue";
+import CreateProtocol from "./Shared/Forms/CreateProtocol.vue";
+export default {
+    components: {
+        Layout,
+        CreateProtocol,
+    },
+    props: {
+        people: Array,
+    },
+};
+</script>
