ae7f70507a4ad72b9877abff621024f8df772e63
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-16 15:47:04 -0300

    style: change edit and delete button style

--- resources/js/Pages/Shared/PersonCard.vue
+++ resources/js/Pages/Shared/PersonCard.vue
@@ -1,10 +1,13 @@
 <template>
     <div
-        class="grid grid-rows-6 space-y-0.5 items-center justify-items-start border-2 border-zinc-800 rounded m-1 p-4 bg-zinc-100 font-normal text-xl hover:bg-zinc-200/80 sm:grid-cols-6 sm:grid-rows-none"
+        class="grid grid-rows-6 space-y-0.5 items-start justify-items-start border-2 border-zinc-800 rounded m-1 p-4 bg-zinc-100 font-normal text-xl hover:bg-zinc-200/80 sm:grid-cols-6 sm:grid-rows-none"
     >
         <span><strong>CPF: </strong>{{ person.cpf }}</span>
         <span><strong>Nome: </strong>{{ person.name }}</span>
-        <span><strong>Nascimento: </strong>{{ person.birthday }}</span>
+        <span
+            ><strong>Nascimento: </strong
+            >{{ formatDate(person.birthday) }}</span
+        >
         <span><strong>Sexo: </strong>{{ person.sex }}</span>
         <span
             ><strong>Endereço: </strong>
@@ -19,16 +22,16 @@
                     .join(", ")
             }}
         </span>
-        <div class="flex justify-center items-center w-full gap-4">
+        <div class="flex justify-center items-center w-full h-full gap-4">
             <nav-link
-                class="font-medium text-amber-500 hover:underline"
+                class="font-medium bg-amber-500 p-2 rounded-sm text-white hover:bg-amber-600"
                 :href="route('edit-person', { id: person.id })"
                 as="button"
             >
                 Editar
             </nav-link>
             <nav-link
-                class="font-medium text-rose-500 hover:underline"
+                class="font-medium bg-rose-500 p-2 rounded-sm text-white hover:bg-rose-600"
                 :href="route('delete-person', { id: person.id })"
                 as="button"
                 method="delete"
@@ -45,6 +48,13 @@ export default {
     components: { NavLink },
     props: {
         person: Object,
+    },
+    methods: {
+        formatDate(date) {
+            const splitDate = date.split("-");
+            const newDate = `${splitDate[2]}/${splitDate[1]}/${splitDate[0]}`;
+            return newDate;
+        },
     },
 };
 </script>
--- resources/js/Pages/Shared/ProtocolCard.vue
+++ resources/js/Pages/Shared/ProtocolCard.vue
@@ -1,21 +1,27 @@
 <template>
     <div
-        class="grid grid-rows-6 space-y-0.5 items-center justify-items-start border-2 border-zinc-800 rounded m-1 p-4 bg-zinc-100 font-normal text-xl hover:bg-zinc-200/80 sm:grid-cols-6 sm:grid-rows-none"
+        class="grid grid-rows-7 space-y-0.5 items-start justify-items-start border-2 border-zinc-800 rounded m-1 p-4 bg-zinc-100 font-normal text-xl hover:bg-zinc-200/80 sm:grid-cols-7 sm:grid-rows-none sm:space-x-0.5"
     >
         <span><strong>ID: </strong>{{ protocol.id }}</span>
         <span><strong>Descrição: </strong>{{ protocol.description }}</span>
         <span><strong>Prazo: </strong>{{ protocol.deadline }}</span>
+        <span
+            ><strong>Cadastrado em: </strong
+            >{{ formatDate(protocol.created_at) }}</span
+        >
         <span><strong>Contribuinte: </strong>{{ protocol.person.name }}</span>
         <span><strong>CPF: </strong>{{ protocol.person.cpf }}</span>
         <div class="flex justify-center items-center w-full gap-4">
             <nav-link
-                class="font-medium text-amber-500 hover:underline"
+                class="font-medium bg-amber-500 p-2 rounded-sm text-white hover:bg-amber-600"
                 as="button"
+                :href="route('edit-protocol', { id: protocol.id })"
+                method="get"
             >
                 Editar
             </nav-link>
             <nav-link
-                class="font-medium text-rose-500 hover:underline"
+                class="font-medium bg-rose-500 p-2 rounded-sm text-white hover:bg-rose-600"
                 as="button"
                 :href="route('delete-protocol', { id: protocol.id })"
                 method="delete"
@@ -32,6 +38,17 @@ export default {
     components: { NavLink },
     props: {
         protocol: Object,
+    },
+    methods: {
+        formatDate(date) {
+            const dateUTC = new Date(Date.parse(date));
+            const localDate = dateUTC.toLocaleDateString("pt-BR", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+            });
+            return localDate;
+        },
     },
 };
 </script>
