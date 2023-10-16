ed21d65b3379a18fd85b76ba53128343fa04241f
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-16 15:45:30 -0300

    feat: add edit protocol page

---
+++ resources/js/Pages/EditProtocol.vue
@@ -0,0 +1,25 @@
+<template>
+    <layout>
+        <div class="flex justify-center">
+            <edit-protocol
+                :protocol="protocol"
+                :people="people"
+            ></edit-protocol>
+        </div>
+    </layout>
+</template>
+<script>
+import Layout from "./Shared/Layout.vue";
+import EditProtocol from "./Shared/Forms/EditProtocol.vue";
+
+export default {
+    components: {
+        Layout,
+        EditProtocol,
+    },
+    props: {
+        protocol: Object,
+        people: Array,
+    },
+};
+</script>
