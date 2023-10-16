5931c56f7c43568e929ee1f66579fc00eca69bb6
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-16 15:46:31 -0300

    feat: add search icon

--- resources/js/Pages/Shared/Icon.vue
+++ resources/js/Pages/Shared/Icon.vue
@@ -93,6 +93,22 @@
         <polyline points="16 17 21 12 16 7"></polyline>
         <line x1="21" y1="12" x2="9" y2="12"></line>
     </svg>
+    <svg
+        v-if="name === 'search'"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-search"
+    >
+        <circle cx="11" cy="11" r="8"></circle>
+        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+    </svg>
 </template>
 <script>
 export default {
