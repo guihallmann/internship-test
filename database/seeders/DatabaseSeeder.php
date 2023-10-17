8ae58b0bee3bc9c986136a8bda12d45ca3c87bdd
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-17 13:18:26 -0300

    feat: create user seeders for db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'TI',
+            'email' => 'ti@email.com',
+            'cpf' => '03565915064',
+            'role' => 'Ti',
+            'password' => 'tititi',
+            'email_verified_at' => now(),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Sys',
+            'email' => 'sys@email.com',
+            'cpf' => '88817655569',
+            'role' => 'Sys',
+            'password' => 'syssys',
+            'email_verified_at' => now(),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Op',
+            'email' => 'op@email.com',
+            'cpf' => '12345678999',
+            'role' => 'Op',
+            'password' => 'opopop',
+            'email_verified_at' => now(),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
