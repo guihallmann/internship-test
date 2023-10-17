91945e915adebff7db3494be602d6fe3569b0663
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-17 13:23:27 -0300

    feat: add edit user page

---
+++ resources/js/Pages/User/Edit.vue
@@ -0,0 +1,21 @@
+<template>
+    <layout>
+        <div class="flex justify-center">
+            <edit-user :user="user" />
+        </div>
+    </layout>
+</template>
+<script>
+import Layout from "../Shared/Layout.vue";
+import EditUser from "../Shared/Forms/EditUser.vue";
+
+export default {
+    components: {
+        Layout,
+        EditUser,
+    },
+    props: {
+        user: Object,
+    },
+};
+</script>
