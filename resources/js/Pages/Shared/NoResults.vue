18de776def82853182dcb47c5fb4cdb4c2c060ed
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-17 13:21:59 -0300

    feat: create no result found component

---
+++ resources/js/Pages/Shared/NoResults.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <span
+        v-if="!size"
+        class="flex justify-center items-center p-10 text-lg text-justify text-zinc-800 font-bold sm:text-3xl"
+    >
+        Nenhum resultado encontrado...
+    </span>
+</template>
+<script setup>
+const props = defineProps({
+    size: Number,
+});
+</script>
