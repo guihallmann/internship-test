c3ba3f1bc00d27846cd7b517c8511375f56dffa9
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-17 13:19:38 -0300

    feat: remove cards from home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,26 +1,6 @@
 <template>
-    <layout>
-        <div class="flex flex-col justify-center items-center p-4 m-2">
-            <card href="/create-person">
-                Novo Contribuinte
-                <icon name="add-user" />
-            </card>
-            <card href="/create-protocol">
-                Novo Protocolo
-                <icon name="add-prot" />
-            </card>
-        </div>
-    </layout>
+    <layout />
 </template>
-<script>
-import Card from "./Shared/Card.vue";
+<script setup>
 import Layout from "./Shared/Layout.vue";
-import Icon from "./Shared/Icon.vue";
-export default {
-    components: {
-        Card,
-        Layout,
-        Icon,
-    },
-};
 </script>
