d5da24f559e3d1b02a5d5da32020c811b37d3a05
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-17 13:21:47 -0300

    feat: add link to users

--- resources/js/Pages/Shared/Navbar.vue
+++ resources/js/Pages/Shared/Navbar.vue
@@ -8,82 +8,47 @@
         <div class="hidden sm:flex space-x-8">
             <nav-link
                 class="flex justify-between items-center w-full gap-2 text-xl text-white font-medium hover:underline"
-                href="/people"
-                :active="$page.component === 'People'"
+                href="/person/all"
+                :active="$page.component === 'Person/Index'"
             >
                 Contribuintes
                 <icon name="users" />
             </nav-link>
             <nav-link
                 class="flex justify-between items-center w-full gap-2 text-xl text-white font-medium hover:underline"
-                href="/protocols"
-                :active="$page.component === 'Protocols'"
+                href="/protocol/all"
+                :active="$page.component === 'Protocol/Index'"
             >
                 Protocolos
                 <icon name="prots" />
             </nav-link>
             <nav-link
                 class="flex justify-between items-center w-full gap-2 text-xl text-white font-medium hover:underline"
-                href="/logout"
-                method="post"
-                as="button"
+                href="/user/all"
+                :active="$page.component === 'User/Index'"
             >
-                Sair
-                <icon name="logout" />
+                Usuários
+                <icon name="users" />
             </nav-link>
         </div>
 
+        <nav-link
+            class="hidden sm:flex items-center justify-center gap-2 text-xl text-white font-medium hover:underline"
+            href="/logout"
+            method="post"
+            as="button"
+        >
+            Sair
+            <icon name="logout" />
+        </nav-link>
         <div class="sm:hidden">
-            <button
-                @click="isBurgerMenuOpen = !isBurgerMenuOpen"
-                class="text-white text-xl font-bold"
-            >
-                ☰
-            </button>
-            <div
-                v-if="isBurgerMenuOpen"
-                class="absolute top-14 right-4 border-2 border-zinc-800 bg-zinc-100 rounded p-4 shadow-md space-y-4 justify-between items-center flex flex-col"
-            >
-                <nav-link
-                    class="flex justify-between items-center w-full gap-2 text-xl text-zinc-800 font-medium hover:underline"
-                    href="/people"
-                    :active="$page.component === 'People'"
-                >
-                    Contribuintes
-                    <icon name="users" />
-                </nav-link>
-                <nav-link
-                    class="flex justify-between items-center w-full gap-2 text-xl text-zinc-800 font-medium hover:underline"
-                    href="/protocols"
-                    :active="$page.component === 'Protocols'"
-                >
-                    Protocolos
-                    <icon name="prots" />
-                </nav-link>
-                <nav-link
-                    class="flex justify-between items-center w-full gap-2 text-xl text-zinc-800 font-medium hover:underline"
-                    href="/logout"
-                    method="post"
-                    as="button"
-                >
-                    Sair
-                    <icon name="logout" />
-                </nav-link>
-            </div>
+            <burguer />
         </div>
     </nav>
 </template>
 
-<script>
+<script setup>
 import NavLink from "./NavLink.vue";
 import Icon from "./Icon.vue";
-
-export default {
-    components: { NavLink, Icon },
-    data() {
-        return {
-            isBurgerMenuOpen: false,
-        };
-    },
-};
+import Burguer from "./Burguer.vue";
 </script>
