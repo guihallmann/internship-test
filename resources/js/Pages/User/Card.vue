4cc716e0a0a5aefd1562cb722aa830833b4b1173
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-17 13:23:07 -0300

    feat: add create user page and card

---
+++ resources/js/Pages/User/Card.vue
@@ -0,0 +1,53 @@
+<template>
+    <div
+        class="grid grid-rows-6 space-y-0.5 items-start justify-items-start border-2 border-zinc-800 rounded m-1 p-4 bg-zinc-100 font-normal text-xl hover:bg-zinc-200/80 sm:grid-cols-6 sm:grid-rows-none sm:space-x-0.5"
+    >
+        <span><strong>Nome: </strong>{{ user.name }}</span>
+        <span><strong>Email: </strong>{{ user.email }}</span>
+        <span><strong>CPF: </strong>{{ user.cpf }}</span>
+        <span><strong>Cargo: </strong>{{ user.role }}</span>
+        <span
+            ><strong>Cadastrado em: </strong
+            >{{ formatDate(user.created_at) }}</span
+        >
+        <div class="flex justify-center items-center w-full gap-4">
+            <nav-link
+                class="font-medium bg-amber-500 p-2 rounded-sm text-white hover:bg-amber-600"
+                as="button"
+                :href="route('edit-user', { id: user.id })"
+                method="get"
+            >
+                Editar
+            </nav-link>
+            <nav-link
+                class="font-medium bg-rose-500 p-2 rounded-sm text-white hover:bg-rose-600"
+                as="button"
+                :href="route('destroy-user', { id: user.id })"
+                method="delete"
+            >
+                Excluir
+            </nav-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import NavLink from "../Shared/NavLink.vue";
+export default {
+    components: { NavLink },
+    props: {
+        user: Object,
+    },
+    methods: {
+        formatDate(date) {
+            const dateUTC = new Date(Date.parse(date));
+            const localDate = dateUTC.toLocaleDateString("pt-BR", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+            });
+            return localDate;
+        },
+    },
+};
+</script>
---
+++ resources/js/Pages/User/Create.vue
@@ -0,0 +1,18 @@
+<template>
+    <layout>
+        <div class="flex justify-center">
+            <create-user />
+        </div>
+    </layout>
+</template>
+<script>
+import Layout from "../Shared/Layout.vue";
+import CreateUser from "../Shared/Forms/CreateUser.vue";
+
+export default {
+    components: {
+        Layout,
+        CreateUser,
+    },
+};
+</script>
