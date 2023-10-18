5666c6f78166bf4a491242b8059b153be13399ae
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-18 15:05:40 -0300

    feat: add departments icon

--- resources/js/Pages/Shared/Icon.vue
+++ resources/js/Pages/Shared/Icon.vue
@@ -109,6 +109,24 @@
         <circle cx="11" cy="11" r="8"></circle>
         <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
     </svg>
+
+    <svg
+        v-if="name === 'deps'"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-trello"
+    >
+        <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+        <rect x="7" y="7" width="3" height="9"></rect>
+        <rect x="14" y="7" width="3" height="5"></rect>
+    </svg>
 </template>
 <script>
 export default {
