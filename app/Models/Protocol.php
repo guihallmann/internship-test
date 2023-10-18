e8885cdb4668c776c610eced9c0c56228816314c
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-18 15:03:12 -0300

    feat: add departments relationships with other models

--- app/Models/Protocol.php
+++ app/Models/Protocol.php
@@ -4,14 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Protocol extends Model
 {
     use HasFactory;
 
-    public function person()
+    public function person(): BelongsTo
     {
         return $this->belongsTo(Person::class);
+    }
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
     }
     
     protected $table = 'protocols';
