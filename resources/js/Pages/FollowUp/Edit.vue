df49f784afbefe6d2575f02dddef317a65c8f8c0
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-27 12:25:57 -0300

    feat: edit followUp page

---
+++ resources/js/Pages/FollowUp/Edit.vue
@@ -0,0 +1,15 @@
+<template>
+    <layout>
+        <div class="flex flex-col justify-center">
+            <edit-follow-up :followUp="followUp" />
+        </div>
+    </layout>
+</template>
+<script setup>
+import Layout from "../Shared/Layout.vue";
+import EditFollowUp from "../Shared/Forms/EditFollowUp.vue";
+
+const props = defineProps({
+    followUp: Object,
+});
+</script>
