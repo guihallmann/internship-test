66b6314e3125e8ddf272a993602560b0bf2b4280
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-27 12:26:22 -0300

    feat: followUps index

---
+++ resources/js/Pages/FollowUp/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <layout>
+        <no-results :size="followUps.length" />
+        <div class="flex flex-col mt-4">
+            <card
+                v-for="followUp in props.followUps"
+                :key="followUp.id"
+                :followUp="followUp"
+            />
+        </div>
+    </layout>
+</template>
+<script setup>
+import Layout from "../Shared/Layout.vue";
+import NoResults from "../Shared/NoResults.vue";
+import Card from "./Card.vue";
+const props = defineProps({
+    followUps: Array,
+});
+</script>
