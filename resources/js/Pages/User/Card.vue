05cd7ec44944d869b847a0b5fa7ecbb3f3ac3357
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-10-30 15:38:43 -0300

    feat: add modal to confirm delete or remove user + notifications

--- resources/js/Pages/User/Card.vue
+++ resources/js/Pages/User/Card.vue
@@ -20,39 +20,70 @@
             >
                 Editar
             </nav-link>
-            <nav-link
+            <button
                 v-if="!removeButton"
-                class="font-medium bg-rose-500 p-2 rounded-sm text-white hover:bg-rose-600"
-                as="button"
-                :href="route('destroy-user', { id: user.id })"
-                method="delete"
+                class="font-medium bg-rose-500 p-2 rounded-sm text-white hover:bg-rose-600 shadow-md"
+                type="button"
+                @click="showModal = !showModal"
             >
                 Excluir
-            </nav-link>
-            <nav-link
+            </button>
+            <button
                 v-if="removeButton"
-                class="font-medium bg-rose-500 p-2 rounded-sm text-white hover:bg-rose-600"
-                as="button"
-                :href="
-                    route('remove-user-from-department', {
-                        user_id: user.pivot.user_id,
-                        department_id: user.pivot.department_id,
-                    })
-                "
-                method="delete"
+                class="font-medium bg-rose-500 p-2 rounded-sm text-white hover:bg-rose-600 shadow-md"
+                type="button"
+                @click="showModal = !showModal"
             >
                 Remover
-            </nav-link>
+            </button>
         </div>
     </div>
+    <delete-modal
+        :remove-button="removeButton"
+        :active="showModal"
+        @cancel="showModal = false"
+        @delete="destroyUser(user.id)"
+        @remove="removeUser(user.pivot.user_id, user.pivot.department_id)"
+    />
 </template>
 
 <script setup>
 import NavLink from "../Shared/NavLink.vue";
+import DeleteModal from "../Shared/DeleteModal.vue";
+import { ref } from "vue";
+import { router } from "@inertiajs/vue3";
 import { formatDate } from "../../utils/date";
+import { useToast } from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-sugar.css";
+
+const toast = useToast();
+
+const showModal = ref(false);
 
 const props = defineProps({
     user: Object,
     removeButton: Boolean,
 });
+
+const destroyUser = (id) => {
+    router.delete(route("destroy-user", id), {
+        onSuccess: () => {
+            toast.error("Usuário excluído com sucesso!");
+        },
+    });
+};
+
+const removeUser = (userId, departmentId) => {
+    router.delete(
+        route("remove-user-from-department", {
+            user_id: userId,
+            department_id: departmentId,
+        }),
+        {
+            onSuccess: () => {
+                toast.error("Usuário removido do departamento com sucesso!");
+            },
+        }
+    );
+};
 </script>
