91a046fb65bc71696ecee211711845ceaebe3d23
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-11-06 12:29:51 -0300

    ref: changing departments pages to use dt

--- resources/js/Pages/Department/Index.vue
+++ resources/js/Pages/Department/Index.vue
@@ -1,52 +1,158 @@
 <template>
-    <Layout>
-        <div
-            class="flex flex-col justify-center items-center gap-2 my-4 mx-1 sm:flex-row sm:w-1/2 sm:mx-auto"
-        >
-            <input
-                v-model="search"
-                name="search"
-                type="text"
-                placeholder="Pesquisar por nome ou CPF..."
-                class="w-full p-3 rounded bg-zinc-100 focus:outline-none focus:bg-emerald-100 border-2 border-zinc-800"
-            />
-            <Link
-                class="w-full p-3 border-2 border-zinc-800 rounded text-lg text-center font-bold bg-emerald-400 hover:bg-emerald-400/80"
-                href="/department/create"
+    <layout>
+        <div class="container mt-4 mx-auto">
+            <div class="flex justify-center space-x-4 my-2">
+                <button
+                    class="font-medium bg-cyan-500 p-2 rounded-sm text-white hover:bg-cyan-600 shadow-md"
+                    type="button"
+                    @click="showDepartmentDetails"
+                >
+                    Detalhes
+                </button>
+                <button
+                    class="font-medium p-2 rounded-sm text-white shadow-md bg-amber-500 hover:bg-amber-600"
+                    type="button"
+                    @click="editDepartment"
+                >
+                    Editar
+                </button>
+                <button
+                    class="font-medium bg-rose-500 p-2 rounded-sm text-white hover:bg-rose-600 shadow-md"
+                    type="button"
+                    @click="shouldOpenModal"
+                >
+                    Excluir
+                </button>
+                <Link
+                    class="text-center font-medium bg-emerald-500 p-2 rounded-sm text-white hover:bg-emerald-600 shadow-md"
+                    href="/department/create"
+                >
+                    Novo
+                </Link>
+            </div>
+            <DataTable
+                id="datatable"
+                :data="departments"
+                :columns="columns"
+                :options="options"
+                ref="table"
+                class="display nowrap"
+                width="100%"
             >
-                Novo Departamento
-            </Link>
+                <thead class="bg-gray-200">
+                    <tr>
+                        <th>Nome</th>
+                        <th>Cadastrado</th>
+                        <th>Usuários</th>
+                        <th>Protocolos</th>
+                    </tr>
+                </thead>
+            </DataTable>
         </div>
-        <no-results :size="departments.length" />
-        <div class="grid grid-cols-1 sm:grid-cols-4 w-full">
-            <card
-                v-for="department in props.departments"
-                :key="department.id"
-                :department="department"
-            />
-        </div>
-    </Layout>
+    </layout>
+    <delete-modal
+        :active="showModal"
+        @cancel="showModal = false"
+        @delete="destroyDepartment"
+    />
 </template>
-
 <script setup>
+import DataTable from "datatables.net-vue3";
+import DataTablesCore from "datatables.net";
+import "datatables.net-select";
+import "datatables.net-responsive";
 import Layout from "../Shared/Layout.vue";
-import NoResults from "../Shared/NoResults.vue";
-import Card from "./Card.vue";
-import { Link } from "@inertiajs/vue3";
-import { ref, watch } from "vue";
+import DeleteModal from "../Shared/DeleteModal.vue";
+import { ref, onMounted } from "vue";
+import { formatDate } from "../../utils/date";
 import { router } from "@inertiajs/vue3";
+import { useToast } from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-sugar.css";
+import { Link } from "@inertiajs/vue3";
+
+DataTable.use(DataTablesCore);
 
 const props = defineProps({
     departments: Array,
-    filters: Object,
 });
 
-let search = ref(props.filters.search);
-watch(search, (value) => {
-    router.get(
-        "/department/all",
-        { search: value },
-        { preserveState: true, replace: true }
-    );
+const toast = useToast();
+
+const showModal = ref(false);
+
+let dt;
+
+const table = ref();
+
+onMounted(function () {
+    dt = table.value.dt;
 });
+
+const columns = [
+    { data: "name" },
+    { data: null, render: (data) => formatDate(data.created_at) },
+    { data: null, render: (data) => data.users_count },
+    { data: null, render: (data) => data.protocols_count },
+];
+
+const options = {
+    autoWidth: true,
+    responsive: true,
+    select: "single",
+    select: {
+        info: false,
+    },
+    language: {
+        paginate: {
+            first: "Primeira",
+            last: "Última",
+            next: "Próxima",
+            previous: "Anterior",
+        },
+        search: "Pesquisar",
+        emptyTable: "Sem dados disponíveis",
+        zeroRecords: "Nenhum resultado encontrado",
+        info: "Mostrando de _START_ a _END_ de _TOTAL_ registros",
+        infoEmpty: "Mostrando 0 de 0 registros",
+        infoFiltered: "(filtrados do total de _MAX_ registros)",
+        lengthMenu: "Mostrar: _MENU_",
+    },
+};
+
+const getSelectedRowId = () => {
+    const row = dt.row({ selected: true }).data();
+    if (row) return row.id;
+    else return null;
+};
+
+const showDepartmentDetails = () => {
+    const id = getSelectedRowId();
+    if (!id) toast.warning("Selecione um departamento!");
+    else router.get(route("show-department", id));
+};
+
+const editDepartment = () => {
+    const id = getSelectedRowId();
+    if (!id) toast.warning("Selecione um departamento!");
+    else router.get(route("edit-department", id));
+};
+
+const destroyDepartment = () => {
+    const id = getSelectedRowId();
+    router.delete(route("destroy-department", id), {
+        onSuccess: () => {
+            toast.error("Departamento excluído com sucesso!");
+            showModal.value = false;
+        },
+    });
+};
+
+const shouldOpenModal = () => {
+    const isRowSelected = getSelectedRowId();
+    if (isRowSelected) {
+        showModal.value = true;
+    } else {
+        toast.error("Selecione um departamento!");
+    }
+};
 </script>
--- resources/js/Pages/Department/Show.vue
+++ resources/js/Pages/Department/Show.vue
@@ -11,26 +11,139 @@
         <div class="p-4">
             <span class="font-bold text-3xl"> Lista de Usuários </span>
             <hr class="flex-grow my-3" />
-            <div class="flex flex-col">
-                <card
-                    v-for="user in props.departmentUsers.users"
-                    :key="user.id"
-                    :user="user"
-                    :remove-button="true"
-                />
+            <div class="container mt-4 mx-auto">
+                <div class="flex justify-center mb-2">
+                    <button
+                        class="font-medium bg-rose-500 p-2 rounded-sm text-white hover:bg-rose-600 shadow-md"
+                        type="button"
+                        @click="shouldOpenModal"
+                    >
+                        Remover
+                    </button>
+                </div>
+                <DataTable
+                    id="datatable"
+                    :data="departmentUsers.users"
+                    :columns="columns"
+                    :options="options"
+                    ref="table"
+                    class="display nowrap"
+                    width="100%"
+                >
+                    <thead class="bg-gray-200">
+                        <tr>
+                            <th>Nome</th>
+                            <th>Email</th>
+                            <th>CPF</th>
+                            <th>Cargo</th>
+                            <th>Cadastrado</th>
+                        </tr>
+                    </thead>
+                </DataTable>
             </div>
-            <no-results :size="departmentUsers.users.length" />
         </div>
     </Layout>
+    <delete-modal
+        :remove-button="true"
+        :active="showModal"
+        @cancel="showModal = false"
+        @remove="removeUser"
+    />
 </template>
 <script setup>
 import Layout from "../Shared/Layout.vue";
 import AddUser from "./AddUser.vue";
-import Card from "../User/Card.vue";
-import NoResults from "../Shared/NoResults.vue";
+import DataTable from "datatables.net-vue3";
+import DataTablesCore from "datatables.net";
+import "datatables.net-select";
+import "datatables.net-responsive";
+import DeleteModal from "../Shared/DeleteModal.vue";
+import { ref, onMounted } from "vue";
+import { formatDate } from "../../utils/date";
+import { formatCpf } from "../../utils/cpf";
+import { router } from "@inertiajs/vue3";
+import { useToast } from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-sugar.css";
+
+const toast = useToast();
+
+const showModal = ref(false);
 
 const props = defineProps({
     departmentUsers: Object,
     users: Array,
 });
+
+DataTable.use(DataTablesCore);
+
+let dt;
+
+const table = ref();
+
+onMounted(function () {
+    dt = table.value.dt;
+});
+
+const columns = [
+    { data: "name" },
+    { data: "email" },
+    { data: null, render: (data) => formatCpf(data.cpf) },
+    { data: null, render: (data) => data.role.toUpperCase() },
+    { data: null, render: (data) => formatDate(data.created_at) },
+];
+
+const options = {
+    autoWidth: true,
+    responsive: true,
+    select: "single",
+    select: {
+        info: false,
+    },
+    language: {
+        paginate: {
+            first: "Primeira",
+            last: "Última",
+            next: "Próxima",
+            previous: "Anterior",
+        },
+        search: "Pesquisar",
+        emptyTable: "Sem dados disponíveis",
+        zeroRecords: "Nenhum resultado encontrado",
+        info: "Mostrando de _START_ a _END_ de _TOTAL_ registros",
+        infoEmpty: "Mostrando 0 de 0 registros",
+        infoFiltered: "(filtrados do total de _MAX_ registros)",
+        lengthMenu: "Mostrar: _MENU_",
+    },
+};
+
+const getSelectedRow = () => {
+    const row = dt.row({ selected: true }).data();
+    if (row) return row;
+    else return null;
+};
+
+const shouldOpenModal = () => {
+    const isRowSelected = getSelectedRow();
+    if (isRowSelected) {
+        showModal.value = true;
+    } else {
+        toast.error("Selecione um usuário!");
+    }
+};
+
+const removeUser = () => {
+    const rowData = getSelectedRow();
+    router.delete(
+        route("remove-user-from-department", {
+            user_id: rowData.pivot.user_id,
+            department_id: rowData.pivot.department_id,
+        }),
+        {
+            onSuccess: () => {
+                toast.error("Usuário removido do departamento com sucesso!");
+            },
+        }
+    );
+    showModal.value = false;
+};
 </script>
