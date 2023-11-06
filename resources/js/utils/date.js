ba6652b9c598bb3e11f32afada3ced97c9a2b9fa
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-11-06 13:18:40 -0300

    feat: add function to calculate expire date

--- resources/js/utils/date.js
+++ resources/js/utils/date.js
@@ -3,3 +3,8 @@ export const formatDate = (date) => {
         timeZone: "UTC",
     });
 };
+
+export const expireDate = (date, days) => {
+    date.setDate(date.getDate() + days);
+    return formatDate(date);
+};
